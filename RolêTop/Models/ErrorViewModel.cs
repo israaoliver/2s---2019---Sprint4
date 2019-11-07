571b652f102d0c31b7553ebb42b59cbe8544c580
israaoliver <isrraaoliveira@gmail.com>
2019-11-07 17:23:54 -0300

    Primeiro

---
+++ RolêTop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RolêTop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
