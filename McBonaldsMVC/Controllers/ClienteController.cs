6a523b7faa4e934846360758786b6acff48bb4d8
israaoliver <isrraaoliveira@gmail.com>
2019-11-08 17:22:33 -0300

    Sextou

---
+++ McBonaldsMVC/Controllers/ClienteController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace McBonaldsMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
